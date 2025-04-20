a42718fcdb98b3d83c9aafc3b29cbd0d35bee748
ImBidou <mrbidoupro@gmail.com>
2025-04-21 00:34:01 +0300

    socket.js and server.js new link

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://chkobba-multiplayer-static.onrender.com", // Your React app's origin
+    origin: "www.chkobba.io'", // Your React app's origin
     methods: ["GET", "POST"]
   }
 });
